e7c7f9546b1dd296b49f04391e7f45fa9f4f25b1
ibrahem-io <ibrahem@doandgo.io>
2024-06-27 17:02:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,44 @@
 import streamlit as st
-from langchain.llms import OpenAI
+import openai
 
-st.title('🦜🔗 Quickstart App')
+openai.api_key = "sk-proj-nUkg3EyCCM5uwPaFbMPUT3BlbkFJ3BsTydyWsDqQl1o7iOiq"
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key="sk-proj-nUkg3EyCCM5uwPaFbMPUT3BlbkFJ3BsTydyWsDqQl1o7iOiq")
-  st.info(llm(input_text))
+st.title("My Own ChatGPT!🤖")
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if submitted:
-    generate_response(text)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# initialize model
+if "model" not in st.session_state:
+    st.session_state.model = "gpt-3.5-turbo"
+
+# user input
+if user_prompt := st.chat_input("Your prompt"):
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # generate responses
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        for response in openai.ChatCompletion.create(
+            model=st.session_state.model,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+
+        message_placeholder.markdown(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
