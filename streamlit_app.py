71a2572ff3e20bd0e579155f4d9e5f5ef71264d4
ibrahem-io <ibrahem@doandgo.io>
2024-06-27 16:46:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,12 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = "sk-proj-1LH6p4ZoVbyGSjxrkww9T3BlbkFJN9V0YQU7FdzielolJ3vr"
-
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key="sk-proj-1LH6p4ZoVbyGSjxrkww9T3BlbkFJN9V0YQU7FdzielolJ3vr")
+  llm = OpenAI(temperature=0.7, openai_api_key="sk-proj-nUkg3EyCCM5uwPaFbMPUT3BlbkFJ3BsTydyWsDqQl1o7iOiq")
   st.info(llm(input_text))
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  if submitted and openai_api_key.startswith('sk-'):
+  if submitted:
     generate_response(text)
