d74adc645e07e3bb6dc04ac99a527ea1f7a9d854
ibrahem-io <ibrahem@doandgo.io>
2024-06-27 16:38:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+from langchain.llms import OpenAI
+
+st.title('🦜🔗 Quickstart App')
+
+openai_api_key = st.sidebar.text_input('sk-proj-1LH6p4ZoVbyGSjxrkww9T3BlbkFJN9V0YQU7FdzielolJ3vr')
+
+def generate_response(input_text):
+  self.llm = OpenAI()  # Replace with your OpenAI API key
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
+
+import streamlit as st
 import requests
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
