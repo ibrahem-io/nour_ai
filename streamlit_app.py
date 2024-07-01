97d0f0dffeb5d79d19597955a6bd87a6d1455b08
Ibrahem Aamer <ibrahem@doandgo.io>
2024-07-01 20:57:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,42 +3,7 @@ import openai
 
 openai.api_key = st.secrets["openai"]["api_key"]
 
-st.title("My Own ChatGPT!🤖")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# initialize model
-if "model" not in st.session_state:
-    st.session_state.model = "gpt-3.5-turbo"
-
-# user input
-if user_prompt := st.chat_input("Your prompt"):
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
-    with st.chat_message("user"):
-        st.markdown(user_prompt)
-
-    # generate responses
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-
-        for response in openai.chat.completions.create(
-            model=st.session_state.model,
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-
-        message_placeholder.markdown(full_response)
-
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+st.title("Nour AI!🤖")
+st.set_page_config(
+    layout="wide"
+)
